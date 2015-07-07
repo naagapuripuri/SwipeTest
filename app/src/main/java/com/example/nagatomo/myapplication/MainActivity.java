0eb925da548a70949d3f2ef0c9ed290d576565d0
s1520217 <nagatomo1218@gmail.com>
2015-07-07 17:10:39 +0900

    swipe&tab

--- app/src/main/java/com/example/nagatomo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/nagatomo/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.app.FragmentTransaction;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener{
     android.app.ActionBar.Tab Tab1, Tab2, Tab3;
     MyFragmentStatePagerAdapter mSectionsPagerAdapter;
     ViewPager viewPager;
@@ -23,9 +23,51 @@ public class MainActivity extends FragmentActivity {
         viewPager.setAdapter(mSectionsPagerAdapter);
        // viewPager.setAdapter(new MyFragmentStatePagerAdapter(getSupportFragmentManager()));
 
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+            // Actionbarにタブを追加。
+            // getPageTitleでタブのタイトルを表示
+            actionBar.addTab(actionBar.newTab()
+                    .setText(mSectionsPagerAdapter.getPageTitle(i))
+                    .setTabListener(this));
+        }
+
+        viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+
 
     }
 
 
+
+    /**
+     * タブを選択した時の処理
+     */
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // ここで表示するフラグメントを決定する
+        // setCurrentItem で、下記の SectionPagerAdapter の getItem を呼び出し
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    /**
+     * タブの選択が外れた場合の処理
+     */
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    /**
+     * タブが2度目以降に選択された場合の処理
+     */
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
 
 }
