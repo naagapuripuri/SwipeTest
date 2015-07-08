eb6b8ac3305f6140e1e8f7c16cdc963be4e16c00
s1520217 <nagatomo1218@gmail.com>
2015-07-08 13:08:47 +0900

    NavigationDrawer

--- app/src/main/java/com/example/nagatomo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/nagatomo/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.nagatomo.myapplication;
 
+import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.Fragment;
@@ -9,23 +11,35 @@ import android.app.ActionBar;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.app.FragmentTransaction;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends FragmentActivity implements ActionBar.TabListener{
     android.app.ActionBar.Tab Tab1, Tab2, Tab3;
     MyFragmentStatePagerAdapter mSectionsPagerAdapter;
     ViewPager viewPager;
 
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewPager = (ViewPager) findViewById(R.id.pager);
-        mSectionsPagerAdapter = new MyFragmentStatePagerAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(mSectionsPagerAdapter);
-       // viewPager.setAdapter(new MyFragmentStatePagerAdapter(getSupportFragmentManager()));
 
+        //以下、通常のライブラリのActionBar/及びタブを付ける場合の記述
         final ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
+
+        //以下、ViewPagerの為の記述
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        mSectionsPagerAdapter = new MyFragmentStatePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(mSectionsPagerAdapter);
+        // viewPager.setAdapter(new MyFragmentStatePagerAdapter(getSupportFragmentManager()));
         for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
             // Actionbarにタブを追加。
             // getPageTitleでタブのタイトルを表示
@@ -42,6 +56,36 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
         });
 
 
+        //以下、NavigationDrawerの為の記述
+        String[] list = { "Mercury", "Venus", "Earth", "Mars" };
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer,R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerClosed(View view) {
+                Log.d("ActionBarDrawerToggle", "onDrawerClosed");
+            }
+            @Override
+            public void onDrawerOpened(View drawerView) {
+
+                Log.d("ActionBarDrawerToggle", "onDrawerOpened");
+            }
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+                Log.d("ActionBarDrawerToggle", "onDrawerSlide : " + slideOffset);
+            }
+            @Override
+            public void onDrawerStateChanged(int newState) {
+                Log.d("ActionBarDrawerToggle", "onDrawerStateChanged state : "
+                        + newState);
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerList = (ListView)findViewById(R.id.left_drawer);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, list);
+        // アダプターの設定
+        mDrawerList.setAdapter(adapter);
     }
 
 
@@ -70,4 +114,62 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
     }
 
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+    /*    if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }*/
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
+
+
+
+
+/*
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggle
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+*/
 }
+
+
+
+
+
+
